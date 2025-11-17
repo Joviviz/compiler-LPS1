9b393dd62866e3c548b9ad4574ee81d5cec4b647
Joviviz <101208535+Joviviz@users.noreply.github.com>
2025-11-17 19:38:34 -0300

    Fix get(str) "}"

--- Compilador.java
+++ Compilador.java
@@ -36,12 +36,12 @@ public class Compilador {
 
     // finaliza o codigo C
     private void printarFooter() {
-        System.out.println("gets (str);");
+        System.out.println("  gets (str);");
         System.out.println("}");
     }
 
     // Tokenizar a entrada
-    private void lerProximoToken(){
+    private void lerProximoToken() {
         // pula espacos em branco
         while (index < input.length() - 1 && Character.isWhitespace(lookahead)) {
             index++;
@@ -68,11 +68,12 @@ public class Compilador {
     }
 
     // verificar se o char bate com o lookahead
-    private void match(char expected){
+    private void match(char expected) {
         if (lookahead == expected) {
             lerProximoToken();
         } else {
-            throw new RuntimeException("Erro de sintaxe: esperado '" + expected + "', mas encontrado '" + lookahead + "'");
+            throw new RuntimeException(
+                    "Erro de sintaxe: esperado '" + expected + "', mas encontrado '" + lookahead + "'");
         }
     }
 
@@ -82,29 +83,29 @@ public class Compilador {
         }
     }
 
-
     // VALOR VARIAVEL E NUMERO
-    private String getValor(){
+    private String getValor() {
         if (Character.isLowerCase(lookahead)) {
             return String.valueOf(getVariavel());
-        } else if (Character.isDigit(lookahead)){
+        } else if (Character.isDigit(lookahead)) {
             return String.valueOf(getNumero());
         } else {
-            throw new RuntimeException("Erro de sintaxe: Numero ou Variavel esperado, mas encontrado '" + lookahead + "'");
+            throw new RuntimeException(
+                    "Erro de sintaxe: Numero ou Variavel esperado, mas encontrado '" + lookahead + "'");
         }
     }
 
-    private char getVariavel(){
+    private char getVariavel() {
         if (!Character.isLowerCase(lookahead)) {
             throw new RuntimeException("Variavel (letra minuscula) esperada, mas encontrado '" + lookahead + "'");
         }
         char variavel = lookahead;
         lerProximoToken();
         return variavel;
-     
+
     }
 
-    private char getNumero(){
+    private char getNumero() {
         if (!Character.isDigit(lookahead)) {
             throw new RuntimeException("Numero esperado, mas encontrado '" + lookahead + "'");
         }
@@ -113,14 +114,14 @@ public class Compilador {
         return numero;
     }
 
-
     // LER COMPARACAO E OPERADORES
-    public String getComparacao(){
+    public String getComparacao() {
         char variavel = getVariavel();
         String operador = getOperador();
         String valor = getValor();
         return variavel + " " + operador + " " + valor;
     }
+
     private String getOperador() {
         char operador = lookahead;
         if (operador == '=') {
@@ -139,50 +140,56 @@ public class Compilador {
 
     // COMANDOS DA LINGUAGEM
     // AssignCommand
-    private void comandoAtribuir(){
+    private void comandoAtribuir() {
         match('=');
         char variavel = getVariavel();
         String valor = getValor();
         System.out.println(variavel + " = " + valor + ";");
     }
+
     // GetCommand
-    private void comandoLer(){
+    private void comandoLer() {
         match('G');
         char variavel = getVariavel();
-        System.out.println("{ gets(str); }");
-        System.out.println("sscanf(str, \"%d\", &" + variavel + " );");
+        System.out.println("{ gets(str);");
+        System.out.println("    sscanf(str, \"%d\", &" + variavel + " );");
+        System.out.println("  }");
     }
-    
+
     // Comandos de matematica basica
-    private void comandoSomar(){
+    private void comandoSomar() {
         match('+');
         char variavel = getVariavel();
         String valor_1 = getValor();
         String valor_2 = getValor();
         System.out.println(variavel + " = " + valor_1 + " + " + valor_2 + ";");
     }
-    private void comandoSubtrair(){
+
+    private void comandoSubtrair() {
         match('-');
         char variavel = getVariavel();
         String valor_1 = getValor();
         String valor_2 = getValor();
         System.out.println(variavel + " = " + valor_1 + " - " + valor_2 + ";");
     }
-    private void comandoMultiplicar(){
+
+    private void comandoMultiplicar() {
         match('*');
         char variavel = getVariavel();
         String valor_1 = getValor();
         String valor_2 = getValor();
         System.out.println(variavel + " = " + valor_1 + " * " + valor_2 + ";");
     }
-    private void comandoDividir(){
+
+    private void comandoDividir() {
         match('/');
         char variavel = getVariavel();
         String valor_1 = getValor();
         String valor_2 = getValor();
         System.out.println(variavel + " = " + valor_1 + " / " + valor_2 + ";");
     }
-    private void comandoModulo(){
+
+    private void comandoModulo() {
         match('%');
         char variavel = getVariavel();
         String valor_1 = getValor();
@@ -192,13 +199,14 @@ public class Compilador {
 
     // COMANDOS DIVERSOS
     // PrintCommand
-    private void comandoPrint(){
+    private void comandoPrint() {
         match('P');
         String valor = getValor();
         System.out.println("printf(\"%d\\n\", " + valor + ");");
     }
+
     // IfCommand
-    private void comandoIf(){
+    private void comandoIf() {
         match('I');
         String condicao = getComparacao();
         System.out.println("if( " + condicao + " ) {");
@@ -208,31 +216,33 @@ public class Compilador {
 
         System.out.println("}");
     }
+
     // WhileCommand
-    private void comandoWhile(){
+    private void comandoWhile() {
         match('W');
         String condicao = getComparacao();
-        System.out.println("while ( " + condicao + "){");
+        System.out.println("while ( " + condicao + ") {");
 
-        //rodar o comando dentro do while
+        // rodar o comando dentro do while
         comandos();
-        System.out.println("}");
+        System.out.println("  }");
     }
+
     // CompositeCommand
-    private void comandoComposite(){
+    private void comandoComposite() {
         match('{');
-        while (lookahead != '}'){
+        while (lookahead != '}') {
             // colocar o maximo de comandos ate fechar a chave
             comandos();
             if (lookahead == '\0') {
                 throw new RuntimeException("Erro: '}' esperado antes do fim da entrada.");
             }
         }
+        match('}');
     }
 
-
     private void comandos() {
-        switch (lookahead){
+        switch (lookahead) {
             case '=':
                 comandoAtribuir();
                 break;
@@ -276,44 +286,41 @@ public class Compilador {
     }
 
     public static void main(String[] args) {
-        String exemplo1 =
-              "G n"
-            + "G p"
-            + "= i 0"
-            + "W i # n {"
-            + "  * a p i"
-            + "  P a"
-            + "  + i i 1"
-            + "}";
-        String exemplo2 =
-              "G n"
-            + "= i 2"
-            + "% a n i"
-            + "W i < n {"
-            + "  I a = 0 = i n"
-            + "  + i i 1"
-            + "  % a n i"
-            + "}"
-            + "I a = 0 P 0"
-            + "I a # 0 P 1";
+        String exemplo1 = "G n"
+                + "G p"
+                + "= i 0"
+                + "W i # n {"
+                + "  * a p i"
+                + "  P a"
+                + "  + i i 1"
+                + "}";
+        String exemplo2 = "G n"
+                + "= i 2"
+                + "% a n i"
+                + "W i < n {"
+                + "  I a = 0 = i n"
+                + "  + i i 1"
+                + "  % a n i"
+                + "}"
+                + "I a = 0 P 0"
+                + "I a # 0 P 1";
 
-        System.out.println("--- INICIO PARTE (A): EXEMPLO 1 ---");
+        System.out.println("=== INICIO EXEMPLO 1 ===");
         try {
             Compilador compilador1 = new Compilador(exemplo1);
             compilador1.compilar();
         } catch (Exception e) {
             System.err.println("Erro ao compilar Exemplo 1: " + e.getMessage());
         }
-        System.out.println("--- FIM PARTE (A): EXEMPLO 1 ---\n");
+        System.out.println("=== FIM EXEMPLO 1 ===\n");
 
-        
-        System.out.println("--- INICIO PARTE (A): EXEMPLO 2 ---");
+        System.out.println("=== INICIO EXEMPLO 2 ===");
         try {
             Compilador compilador2 = new Compilador(exemplo2);
             compilador2.compilar();
         } catch (Exception e) {
             System.err.println("Erro ao compilar Exemplo 2: " + e.getMessage());
         }
-        System.out.println("--- FIM PARTE (A): EXEMPLO 2 ---");
+        System.out.println("=== FIM EXEMPLO 2 ===");
     }
 }
